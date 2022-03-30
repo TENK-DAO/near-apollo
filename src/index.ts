593907b069e1b140c29c1d6a874038fc91b5867d
Willem Wyndham <wyndham@cs.unc.edu>
2022-03-30 10:28:41 -0400

    feat: near future example

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 import fetch from 'cross-fetch';
 import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloProvider,
-  useQuery,
-  gql
+    ApolloClient,
+    InMemoryCache,
+    HttpLink,
+    ApolloProvider,
+    useQuery,
+    gql
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: new HttpLink({uri : 'https://api.thegraph.com/subgraphs/name/r-strawser/mr-brown-project', fetch }),
-  cache: new InMemoryCache()
+    link: new HttpLink({ uri: 'https://api.thegraph.com/subgraphs/name/r-strawser/near-future-nft', fetch }),
+    cache: new InMemoryCache()
 });
 
-client
-  .query({
-    query: gql`
-      query GetContracts {
-          contracts {
+async function combine() {
+    let tokens: any[] = [];
+    for (let i = 0; i < 3000; i += 1000) {
+        let { data } = await client
+            .query({
+                query: gql`{
+          tokens(first: 1000, skip: ${i}) {
             id
-            total_minted
+            ownerId
+            burned
           }
-      }
-    `
-  })
-  .then(result => console.log(result.data));
+        }
+          `
+            });
+        tokens = tokens.concat(data.tokens.filter((t:any) => t.burned === "false").map((t: any) => ({ id: t.id, ownerId: t.ownerId })))
+    }
+    return tokens;
+}
+
+async function main() {
+  let res = await combine();
+  console.log(res)
+}
+
+void main();
